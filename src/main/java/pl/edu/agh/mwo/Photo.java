dd66c94e1619ecafeabcf46f25503cdfecbfe98b
Bartlomiej Bartczak <bartlomiej.bartczak@gmail.com>
2019-05-13 18:25:01 +0200

    Adding attributes and default constructor for class Photo

--- src/main/java/pl/edu/agh/mwo/Photo.java
+++ src/main/java/pl/edu/agh/mwo/Photo.java
@@ -1,5 +1,12 @@
 package pl.edu.agh.mwo;
+import java.util.Scanner;
 
 public class Photo {
-
+    String comment = "";
+    public void Photo (){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Add your comment");
+        String comment = scan.nextLine();
+        System.out.println("Your comment is:" + comment);
+    }
 }
